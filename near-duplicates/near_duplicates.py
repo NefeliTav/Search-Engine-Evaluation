25bc8a459803045c98af5c570173d99405eb1735
GiorgosNik <mirandagioman@gmail.com>
2021-04-05 15:37:37 +0300

    shingles ready

--- near-duplicates/near_duplicates.py
+++ near-duplicates/near_duplicates.py
@@ -1,25 +1,32 @@
 import csv
 import re
+import nltk
+from nltk import ngrams
 
 shingles = {}
+temp = {}
+i = 0
+
 punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
 
 filename = open("../dataset/250K_lyrics_from_MetroLyrics.csv")
 reader = csv.reader(filename, delimiter = ",")
 next(reader)
 for row in reader:
-    i = 0
     lyrics = (re.sub(r'[^\w\s]','',row[5])).lower().split()
-    length = len(lyrics)
+    threegrams = ngrams(lyrics , 3)
 
-    while(i < length - 2):
+    for grams in threegrams:
+        if str(grams) not in temp.keys():
+            temp[str(grams)] = i
+            i += 1
+
         if row[0] in shingles.keys():
-            shingles[row[0]].append([lyrics[i],lyrics[i+1],lyrics[i+2]])
+            if temp[str(grams)] not in shingles[row[0]]:
+                shingles[row[0]].append(temp[str(grams)])
         else:
-            shingles[row[0]] = [[lyrics[i],lyrics[i+1],lyrics[i+2]]]
-        i += 1
-    if row[0] not in shingles.keys():
-        shingles[row[0]] = []
-    #print(shingles[row[0]])
-    #print("----------------------------------------------------------------")
+            shingles[row[0]] = [temp[str(grams)]]
+    print(row[0],shingles[row[0]])
+    print("-------------------------------------------------")
+    s = input()
 filename.close()
